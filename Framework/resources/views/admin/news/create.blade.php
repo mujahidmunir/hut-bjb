e36b309050afeb393893d0fbea6cdebc087e6377
mujahidmunir <mujahidmunir15@gmail.com>
2021-05-04 19:35:26 +0700

    Change Form Create Post

--- Framework/resources/views/admin/news/create.blade.php
+++ Framework/resources/views/admin/news/create.blade.php
@@ -42,7 +42,7 @@
 
                             <div class="form-group">
                                 <label for="select-country1">Thumb Content</label>
-                                <textarea id="redactor" class="description" name="thumb_desc"></textarea>
+                                <textarea  class="description" name="thumb_desc"></textarea>
                             </div>
 
                             <div class="form-group">
