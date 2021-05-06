2b0a7c5202c22380c44be197ce341747c8fd1837
mujahidmunir <mujahidmunir15@gmail.com>
2021-05-07 00:19:36 +0700

    Update Sidebar

--- Framework/resources/views/layouts/assets/banner.blade.php
+++ Framework/resources/views/layouts/assets/banner.blade.php
@@ -1,5 +1,5 @@
 <div class="row">
-    <div class="banner">
+    <div class="banner mt-5">
             <img src="{{URL::to('images/banner/banner-1.png')}}" class="d-block w-100" alt="#">
     </div>
 </div>
