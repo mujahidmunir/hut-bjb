783572fdc185f6f2a499f5c4c3c452e48f837c16
mujahidmunir <mujahidmunir15@gmail.com>
2021-05-07 11:13:33 +0700

    change backround not include Home

--- Framework/resources/views/master/master.blade.php
+++ Framework/resources/views/master/master.blade.php
@@ -4,6 +4,7 @@
 @include('layouts.assets.head')
 
 <body>
+
 <!-- loader Start -->
 <div id="loading">
     <div id="loading-center">
@@ -15,10 +16,12 @@
 <div class="wrapper">
     @include('layouts.assets.header')
     @yield('slider')
+    <div class="" style="overflow: scroll">
     <div class="content-page c-payment-block">
         <div class="container">
             @yield('content')
         </div>
+    </div>
     </div>
 </div>
 <!-- Wrapper End-->
@@ -27,6 +30,7 @@
 @include('sweetalert::alert')
 <!-- Backend Bundle JavaScript -->
 @include('layouts.assets.js')
+
 </body>
 
 </html>
