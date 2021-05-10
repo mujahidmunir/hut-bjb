2191930b589893dd43e21ba411c56e144c31ee6e
mujahidmunir <mujahidmunir15@gmail.com>
2021-05-10 22:27:07 +0700

    Add link Youtube

--- Framework/resources/views/layouts/assets/home.blade.php
+++ Framework/resources/views/layouts/assets/home.blade.php
@@ -190,7 +190,7 @@
 
             <div class="embed-responsive embed-responsive-16by9">
 
-                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/BxV3l090MTQ"
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/EF209GbgKJk"
                             allowfullscreen>
                     </iframe>
 
