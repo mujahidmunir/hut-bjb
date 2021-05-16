7e96a9f3d556136995cb740b464635037e64bee8
mujahidmunir <mujahidmunir15@gmail.com>
2021-05-16 14:02:07 +0700

    test

--- Framework/resources/views/layouts/assets/home.blade.php
+++ Framework/resources/views/layouts/assets/home.blade.php
@@ -188,11 +188,11 @@
             </div>
             <div class="card-body">
                 <div class="embed-responsive embed-responsive-16by9">
-                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/rVmLP6r2k6g"
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/WYzi6409INo"
                             allowfullscreen>
                     </iframe>
                 </div>
-                <a href="https://www.youtube.com/watch?v=rVmLP6r2k6g" target="_blank" class="btn btn-warning btn-block" style="font-size: 2vh">
+                <a href="https://www.youtube.com/watch?v=WYzi6409INo" target="_blank" class="btn btn-warning btn-block" style="font-size: 2vh">
                     <i class="fa fa-youtube-square" style="color: darkred"></i>
                     Play On Youtube
                 </a>
@@ -200,7 +200,7 @@
             <div class="card-footer bg-white">
                 <div class="justify-content-center" style="text-align: justify; font-size: 2vh;">
 
-                    <h4 class="card-title">The Banker Show With Ronal Tike Eps.02</h4>
+                    <h4 class="card-title">The Banker Show With Ronal Tike Eps.01</h4>
                     <p class="mt-3 mb-3">Bersama host @rocknal & @tikeprie pada episode pertama dari The Banker Show akan berbincang bagaimana kita bisa 'survive' dalam badai pandemi yang sudah lebih dari satu tahun menerpa berbagai sendi kehidupan di seluruh dunia.</p>
                     <p>Bahkan dalam obrolan dengan narasumber Direktur Utama bank bjb Bapak @yuddyrenaldi & Financial Planner @aidilakbarmadjid akan diperbincangkan bagaimana cara dan kiat-kiat untuk selain bisa bertahan juga bisa keluar sebagai 'pemenang' dalam menghadapi pandemi.</p>
                     <br>
@@ -237,11 +237,11 @@
             </div>
             <div class="card-body">
                 <div class="embed-responsive embed-responsive-16by9">
-                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/rVmLP6r2k6g"
+                    <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/nb76Ab9Ljz8"
                             allowfullscreen>
                     </iframe>
                 </div>
-                <a href="https://www.youtube.com/watch?v=rVmLP6r2k6g" target="_blank" class="btn btn-warning btn-block" style="font-size: 2vh">
+                <a href="https://www.youtube.com/watch?v=nb76Ab9Ljz8" target="_blank" class="btn btn-warning btn-block" style="font-size: 2vh">
                     <i class="fa fa-youtube-square" style="color: darkred"></i>
                     Play On Youtube
                 </a>
